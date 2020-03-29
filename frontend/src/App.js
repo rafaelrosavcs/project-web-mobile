abba97c40614f57a725f8395251c44069bc714ef
Rafael <rafael_unis@hotmail.com>
2020-03-29 07:59:18 -0300

    criado front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+// JSX (javaScript XML) quando o "HTML" é escrito dentro do arquivo js
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
